abb9e56bd46b5f84095c6aac7f5826a6101c93c3
zhangwenhao <wenhao.zhang@ushow.media>
2022-08-22 18:10:00 +0800

    merge

---
+++ kafka/kfk-readme.md
@@ -0,0 +1,7 @@
+ ### 本地kafka安装与部署:
+ + windows参考https://www.topgoer.com/%E6%95%B0%E6%8D%AE%E5%BA%93%E6%93%8D%E4%BD%9C/go%E6%93%8D%E4%BD%9Ckafka/kafka%E7%9A%84%E5%AE%89%E8%A3%85.html
+ + macos: `brew install kafka` 启动`brew serves start kafka`
+高吞吐量的消息队列
+
+### 第三方库
+ go get github.com/Shopify/sarama
---
+++ kafka/producer/produce.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	//sarama第三方库，kafka的client
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForLocal        //发完数据仅需要leader确认
+	config.Producer.Partitioner = sarama.NewRandomPartitioner //新选出一个partition
+	config.Producer.Return.Successes = true                   //成功交付的message在success chanel返回
+
+	//构造消息
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = "web_log"
+	//连接kafka
+	client, err := sarama.NewSyncProducer([]string{"127.0.0.1:9092"}, config)
+	if err != nil {
+		fmt.Println("producer closed,err:", err)
+		return
+	}
+	defer client.Close()
+	//发送消息
+	for i := 0; i <= 1000; i++ {
+		msg.Value = sarama.StringEncoder(fmt.Sprintf("test%d", i))
+		pid, offset, err := client.SendMessage(msg)
+		if err != nil {
+			fmt.Println("send msg failed,err:", err)
+			return
+		}
+		fmt.Printf("pid:%v offset:%v\n", pid, offset)
+	}
+}
