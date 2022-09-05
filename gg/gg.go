5a1669902a94986f7a1ecd44b8361b29b294aa43
zhangwenhao <wenhao.zhang@ushow.media>
2022-09-05 20:24:38 +0800

    gg

--- gg/gg.go
+++ gg/gg.go
@@ -2,8 +2,107 @@ package main
 
 import (
 	"fmt"
+	"github.com/fogleman/gg"
+	"github.com/nfnt/resize"
+	"github.com/siddontang/go/log"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"os"
 )
 
 func main() {
-	fmt.Println("Test")
+	_ = imageMaker("gg/resource/test2.png")
 }
+
+func drawCircle() {
+	dc := gg.NewContext(1000, 1000)
+	dc.DrawCircle(500, 500, 400)
+	dc.SetRGB255(255, 255, 255)
+	dc.Fill()
+	dc.SavePNG("out.png")
+}
+
+func imageMaker(filePath string) error {
+	//load cover
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var imgTmp image.Image
+	// decode jpeg into image.Image
+	imgTmp, err = png.Decode(file)
+	if err != nil {
+		_ = file.Close()
+		fi, _ := os.Open(filePath)
+		imgTmp, err = jpeg.Decode(fi)
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+	}
+
+	_ = file.Close()
+	w := imgTmp.Bounds().Dx()
+	h := imgTmp.Bounds().Dy()
+	if w != h {
+		return fmt.Errorf("不是正方形")
+	}
+
+	m := resize.Resize(500, 500, imgTmp, resize.Lanczos3)
+	out, err := os.Create("tmp_image.jpeg")
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer out.Close()
+	_ = jpeg.Encode(out, m, nil)
+
+	//load background
+	imgBkg, err := gg.LoadImage("test.png")
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	//draw image
+	dc := gg.NewContext(526, 800)
+	dc.DrawImage(imgBkg, 0, 0)
+	imgCover, err := gg.LoadImage("tmp_image.jpeg")
+	dc.DrawImage(imgCover, 13, 13)
+
+	//保存的制作图片
+	err = dc.SavePNG("test.png")
+	if err != nil {
+		log.Error()
+		return err
+	}
+
+	_ = os.Remove("tmp_image.jpeg")
+	return nil
+}
+
+/*
+methods:
+image:
+DrawImage(im image.Image, x, y int)
+DrawImageAnchored(im image.Image, x, y int, ax, ay float64)
+
+string:
+SetFontFace(fontFace font.Face)
+DrawString(s string, x, y float64)
+DrawStringAnchored(s string, x, y, ax, ay float64)
+
+color:
+SetRGB(r, g, b float64)
+SetRGBA(r, g, b, a float64)
+SetRGB255(r, g, b int)
+
+NewSurfacePattern(im image.Image, op RepeatOp)
+
+Load:
+LoadImage(path string) (image.Image, error)
+LoadPNG(path string) (image.Image, error)
+SavePNG(path string, im image.Image) error
+
+func (a Matrix ) Scale(x, y float64 )矩阵
+*/
--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.18
 require (
 	fyne.io/fyne/v2 v2.2.3
 	github.com/Shopify/sarama v1.36.0
-	github.com/go-sql-driver/mysql v1.5.0
+	github.com/fogleman/gg v1.3.0
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.8
 )
@@ -23,9 +25,9 @@ require (
 	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211213063430-748e38ca8aec // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
@@ -40,7 +42,6 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
-	github.com/mattn/go-sqlite3 v1.14.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
@@ -50,7 +51,7 @@ require (
 	github.com/tevino/abool v1.2.0 // indirect
 	github.com/yuin/goldmark v1.4.0 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
-	golang.org/x/image v0.0.0-20220601225756-64ec528b34cd // indirect
+	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
 	golang.org/x/mobile v0.0.0-20211207041440-4e6c2922fdee // indirect
 	golang.org/x/net v0.0.0-20220809184613-07c6da5e1ced // indirect
 	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
