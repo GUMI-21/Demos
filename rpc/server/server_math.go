51efa9118e9260b34112ba7d79b5483ad9490429
zhangwenhao <wenhao.zhang@ushow.media>
2022-12-21 17:15:53 +0800

    rpc

--- rpc/server/server_math.go
+++ rpc/server/server_math.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"net/rpc"
+)
+
 type ParamsMath struct {
 	a, b int
 }
@@ -9,4 +15,25 @@ type RectMath struct{}
 func (r *RectMath) multi(p ParamsMath, ret *int) error {
 	*ret = p.a * p.b
 	return nil
+}
+
+func (r *RectMath) Division(p ParamsMath, ret *int) error {
+	*ret = p.a / p.b
+	return nil
+}
+
+func (r *RectMath) Remainder(p ParamsMath, ret *int) error {
+	*ret = p.a / p.b
+	return nil
+}
+
+func main() {
+	rect := new(RectMath)
+	rpc.Register(rect)
+	rpc.HandleHTTP()
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Panicln(err)
+	}
+
 }
